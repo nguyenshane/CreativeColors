e34a31fcf446be5ff8ec2a922e8d07ceabcaa792
kdodd <kdodd@ucsc.edu>
2014-05-18 22:43:48 -0700

    UI button images update
    new logo, red/yellow/green/blue buttons with new images, table layout
    slightly altered

--- gen/com/nguyenshane/creativecolors/R.java
+++ gen/com/nguyenshane/creativecolors/R.java
@@ -10,6 +10,9 @@ package com.nguyenshane.creativecolors;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int bg_color=0x7f060000;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -23,25 +26,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int blue_button=0x7f020000;
-        public static final int green_button=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int red_button=0x7f020003;
-        public static final int yellow_button=0x7f020004;
+        public static final int blue_gem=0x7f020001;
+        public static final int green_button=0x7f020002;
+        public static final int green_gem=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int red_button=0x7f020006;
+        public static final int red_gem=0x7f020007;
+        public static final int yellow_button=0x7f020008;
+        public static final int yellow_gem=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080005;
-        public static final int tableRow1=0x7f080000;
-        public static final int tableRow2=0x7f080003;
+        public static final int action_settings=0x7f090006;
+        public static final int button1=0x7f090001;
+        public static final int button2=0x7f090002;
+        public static final int button3=0x7f090004;
+        public static final int button4=0x7f090005;
+        public static final int tableRow1=0x7f090000;
+        public static final int tableRow2=0x7f090003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -71,10 +79,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
