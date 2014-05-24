a2d6291b57025f10958de8631ca8993036069646
Shane Do <nguyenshane@gmail.com>
2014-05-24 10:47:29 -0700

    added push, check pattern now work in logcat

--- src/com/nguyenshane/creativecolors/MainActivity.java
+++ src/com/nguyenshane/creativecolors/MainActivity.java
@@ -6,7 +6,11 @@ package com.nguyenshane.creativecolors;
 import java.util.ArrayList;
 import java.util.Timer;
 
+import org.json.JSONObject;
+import org.json.JSONException;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.LightingColorFilter;
 import android.graphics.Rect;
 import android.media.MediaPlayer;
@@ -24,11 +28,13 @@ import android.widget.Toast;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParsePush;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.ParseAnalytics;
 import com.parse.ParseInstallation;
 import com.parse.PushService;
+import com.parse.SendCallback;
 
 public class MainActivity extends Activity {
 	static final String LOG_TAG = "MainActivity";
@@ -38,9 +44,10 @@ public class MainActivity extends Activity {
 	private ParseQuery<ParseObject> query;
 	private ImageButton ib;
 	private int Rid, Rcontroller, Rglow;
-	private boolean isMyTurn;
+	private boolean isMyTurn, isQuest;
 	private ArrayList<Integer> myArrayButton, oppArrayButton;
-	
+	private String oppChannel;
+
 
 	//Buttons 
 	ImageButton imageButton;
@@ -56,21 +63,20 @@ public class MainActivity extends Activity {
 		PushService.subscribe(this, "pJDqv2DPP4", MainActivity.class);
 		ParseInstallation.getCurrentInstallation().saveInBackground();
 
-		ArrayList<Integer> arrayButton = new ArrayList<Integer>();
-		arrayButton.add(0);
-		arrayButton.add(1);
-		arrayButton.add(2);
-		arrayButton.add(3);
-		arrayButton.add(2);
-		arrayButton.add(3);
-		arrayButton.add(1);
-		arrayButton.add(0);
-		
-		//disableButtons();
-		glowButtonArray(arrayButton,1000);
-		//enableButtons();
-		
-		//glowButton(0,1000);
+		oppArrayButton = new ArrayList<Integer>();
+		myArrayButton = new ArrayList<Integer>();
+		oppArrayButton.add(0);
+		oppArrayButton.add(1);
+		oppArrayButton.add(2);
+		oppArrayButton.add(3);
+
+
+		glowButtonArray(oppArrayButton,1000);
+
+		isQuest = true;
+		isMyTurn = true;
+		oppChannel = "pJDqv2DPP4";
+
 	}
 
 	@Override
@@ -92,104 +98,167 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public void checkTurn(){
 		int status = currentUser.getInt("status");
 		if(status == 2) isMyTurn = true;
 		else isMyTurn = false;
 	}
-	
+
 	public void nextTurn(){
-		if (isMyTurn)
+		if (isMyTurn){
 			currentUser.put("status", 2);
-		else currentUser.put("status", 3);
+			enableButtons();
+		}
+		else { 
+			currentUser.put("status", 3);
+			disableButtons();
+		}
 	}
-	
-	
+
+
 	//setup new game
 	public void newGame(){
 		currentUser = ParseUser.getCurrentUser();
-		if(isMyTurn)
-		currentUser.put("status", 2);
-		currentUser.saveInBackground();
-		
+
+		Intent intent = getIntent();
+		isMyTurn = intent.getBooleanExtra("isMyTurn",true);
+		isQuest = intent.getBooleanExtra("isQuest", true);
+
+	}
+
+	public void pushMyArray(){
+		Log.d(LOG_TAG,"I'm pushing");
+
+		try {
+			JSONObject object = new JSONObject();
+			//object.put("alert", "Alert");
+			//object.put("title", "pushedArrayButton");
+			object.put("action", "pushedArrayButton");   
+			object.put("pushedArrayButton", myArrayButton);
+			ParsePush pushToOpp = new ParsePush();
+			pushToOpp.setData(object);
+			pushToOpp.setChannel(oppChannel);
+
+			pushToOpp.sendInBackground(new SendCallback() {
+				@Override
+				public void done(ParseException e) {
+					if (e != null) ;
+					//Something wrong with push
+				}
+			});
+		} catch (JSONException e) {e.printStackTrace();}
+
+	}
+
+	public void checkPattern(){
+		disableButtons();
+		if(isQuest){
+			if (myArrayButton.size() <= oppArrayButton.size()){
+				if (myArrayButton.get(myArrayButton.size()-1) == oppArrayButton.get(myArrayButton.size()-1)){
+					Log.d(LOG_TAG,"I'm correct");
+				} else	Log.d(LOG_TAG,"I lose");
+			}
+			if (myArrayButton.size() == oppArrayButton.size()+1){
+				pushMyArray();
+			}
+			if (myArrayButton.size() > oppArrayButton.size()+1){
+				Log.d(LOG_TAG,"I lose because I pressed too much");
+			}
+		}
+		else {
+			if (myArrayButton.get(myArrayButton.size()-1) == oppArrayButton.get(myArrayButton.size()-1)){
+				if (myArrayButton.size() == oppArrayButton.size() && myArrayButton.equals(oppArrayButton)) pushMyArray();
+				Log.d(LOG_TAG,"I'm correct");
+			} else Log.d(LOG_TAG,"I lose");
+		}
+		enableButtons();
 	}
 
 	public void onClickButton0(View v){	
 		//Parse Code
-		query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
+		/*query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
 			public void done(ParseObject pObj, ParseException e) {
 				if (e == null) {
 					pObj.put("button", 0);
 					pObj.saveInBackground();
 				}
 			}
-		});
+		});*/
+		myArrayButton.add(0);
+		checkPattern();
+
 	}
 
 	public void onClickButton1(View v){
-		query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
+		/*query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
 			public void done(ParseObject pObj, ParseException e) {
 				if (e == null) {
 					pObj.put("button", 1); //pushing notification to the cloud
 					pObj.saveInBackground();
 				}
 			}
-		});
+		});*/
+		myArrayButton.add(1);
+		checkPattern();
 	}
 
 	public void onClickButton2(View v){
-		query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
+		/*query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
 			public void done(ParseObject pObj, ParseException e) {
 				if (e == null) {
 					pObj.put("button", 2); //pushing notifications to the cloud
 					pObj.saveInBackground();
 				}
 			}
-		});
+		});*/
+		myArrayButton.add(2);
+		checkPattern();
 	}
 
 	public void onClickButton3(View v){
-		query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
+		/*query.getInBackground("gVEyPd7NMM", new GetCallback<ParseObject>() {
 			public void done(ParseObject pObj, ParseException e) {
 				if (e == null) {
 					pObj.put("button", 3);
 					pObj.saveInBackground();
 				}
 			}
-		});
+		});*/
+		myArrayButton.add(3);
+		checkPattern();
 	}
-	
+
 	public void glowButtonArray(final ArrayList<Integer> arrayButton, final long duration){
 		disableButtons();
-		
+
 		CountDownTimer timer2 = new CountDownTimer(duration*(arrayButton.size()+1), duration){
 			int count = 0;
-			
+
 			public void onTick(long remainingTimeMillis){
 				glowButton(arrayButton.get(count), duration);
 				Log.d(LOG_TAG,arrayButton.get(count).toString());
 				count++;
-				
+
 			}
 			public void onFinish(){
 				enableButtons();
 			}
 		}.start();
-		
+
 	}
-	
+
 	public void disableButtons(){
-			ImageButton tmp = (ImageButton) findViewById(R.id.button0);
-			tmp.setClickable(false); tmp.setEnabled(false);
-			tmp = (ImageButton) findViewById(R.id.button1);
-			tmp.setClickable(false); tmp.setEnabled(false);
-			tmp = (ImageButton) findViewById(R.id.button2);
-			tmp.setClickable(false); tmp.setEnabled(false);
-			tmp = (ImageButton) findViewById(R.id.button3);
-			tmp.setClickable(false); tmp.setEnabled(false);
+		ImageButton tmp = (ImageButton) findViewById(R.id.button0);
+		tmp.setClickable(false); tmp.setEnabled(false);
+		tmp = (ImageButton) findViewById(R.id.button1);
+		tmp.setClickable(false); tmp.setEnabled(false);
+		tmp = (ImageButton) findViewById(R.id.button2);
+		tmp.setClickable(false); tmp.setEnabled(false);
+		tmp = (ImageButton) findViewById(R.id.button3);
+		tmp.setClickable(false); tmp.setEnabled(false);
 	}
-	
+
 	public void enableButtons(){
 		ImageButton tmp = (ImageButton) findViewById(R.id.button0);
 		tmp.setClickable(true); tmp.setEnabled(true);
@@ -199,36 +268,35 @@ public class MainActivity extends Activity {
 		tmp.setClickable(true); tmp.setEnabled(true);
 		tmp = (ImageButton) findViewById(R.id.button3);
 		tmp.setClickable(true); tmp.setEnabled(true);
-}
-
+	}
 
 	public void glowButton(int buttonId, long duration){
 		switch(buttonId) {
-			case 0: Rid = R.id.button0; 
-					Rcontroller = R.drawable.green_button_controller;
-					Rglow = R.drawable.green_gem_glow;
-					break;
-			case 1: Rid = R.id.button1; 
-					Rcontroller = R.drawable.yellow_button_controller;
-					Rglow = R.drawable.yellow_gem_glow;
-					break;
-			case 2: Rid = R.id.button2; 
-					Rcontroller = R.drawable.blue_button_controller;
-					Rglow = R.drawable.blue_gem_glow;
-					break;
-			case 3: Rid = R.id.button3; 
-					Rcontroller = R.drawable.red_button_controller;
-					Rglow = R.drawable.red_gem_glow;
-					break;
-			default:Rid = R.id.button0; 
-					Rcontroller = R.drawable.green_button_controller;
-					Rglow = R.drawable.green_gem_glow;
-					break;
+		case 0: Rid = R.id.button0; 
+		Rcontroller = R.drawable.green_button_controller;
+		Rglow = R.drawable.green_gem_glow;
+		break;
+		case 1: Rid = R.id.button1; 
+		Rcontroller = R.drawable.yellow_button_controller;
+		Rglow = R.drawable.yellow_gem_glow;
+		break;
+		case 2: Rid = R.id.button2; 
+		Rcontroller = R.drawable.blue_button_controller;
+		Rglow = R.drawable.blue_gem_glow;
+		break;
+		case 3: Rid = R.id.button3; 
+		Rcontroller = R.drawable.red_button_controller;
+		Rglow = R.drawable.red_gem_glow;
+		break;
+		default:Rid = R.id.button0; 
+		Rcontroller = R.drawable.green_button_controller;
+		Rglow = R.drawable.green_gem_glow;
+		break;
 		}
 
 		ib = (ImageButton) findViewById(Rid);
 		ib.setBackgroundResource(Rglow);
-		
+
 		CountDownTimer timer = new CountDownTimer(duration-5, duration-5){
 			public void onTick(long remainingTimeMillis){}
 			public void onFinish(){
