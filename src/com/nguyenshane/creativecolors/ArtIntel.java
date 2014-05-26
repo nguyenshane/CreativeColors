9381b97ec1a886be34e7885ac4eed88a83cc87ec
jchiu6 <jchiu6@ucsc.edu>
2014-05-25 23:27:22 -0700

    jacky artintel
    hello guys

---
+++ src/com/nguyenshane/creativecolors/ArtIntel.java
@@ -0,0 +1,26 @@
+package com.nguyenshane.creativecolors;
+
+import java.util.Random;
+
+import com.nguyenshane.creativecolors.MainActivity.Colors;
+
+public class ArtIntel {
+	
+	public int generateRandom(Colors[] colors) {
+		//int[] randomArray = new int [n];
+		for (int i = 0; i < colors.length; i++) {
+			int randomArray = Colors.getRandom();
+			//colors[i] = MainActivity.getRandom();
+		}
+		return randomArray;
+	}
+	
+	
+	public static int getRandom() {
+		//java.util.Random r = new java.util.Random(1);
+		Random generator = new Random();
+		int i = generator.nextInt(3) + 1;
+		return i;
+	}
+
+}
